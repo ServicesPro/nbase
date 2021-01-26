e3da99f57c65ad71406214bc7d6195fd07027954
Stéphane KUMA <kumastephane@gmail.com>
2021-01-26 11:38:33 +0000

    firebase_core, firebase_auth, firebase_storage dependencies added to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'pages/splash.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -13,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        
+
       ],
       child: MaterialApp(
         title: 'N-BASE',
