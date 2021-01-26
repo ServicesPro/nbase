c2ab9befa77a9ef0eeedb3884f05c4b6aea850d1
Stéphane KUMA <kumastephane@gmail.com>
2021-01-26 10:12:15 +0000

    Create constant file and Splash screen page added to project

---
+++ lib/helpers/constants.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+const Color kYellow = Color(0xffFDC054);
+const Color kMediumYellow = Color(0xffFDB846);
+const Color kDarkYellow = Color(0xffE99E22);
+const Color kTransparentYellow = Color.fromRGBO(253, 184, 70, 0.7);
+const Color kDarkGrey = Color(0xff202020);
+
+const LinearGradient kMainButton = LinearGradient(
+  colors: [
+    Color.fromRGBO(236, 60, 3, 1),
+    Color.fromRGBO(234, 60, 3, 1),
+    Color.fromRGBO(216, 78, 16, 1),
+  ],
+  begin: FractionalOffset.topCenter,
+  end: FractionalOffset.bottomCenter,
+);
+
+const List<BoxShadow> kShadow = [
+  BoxShadow(
+    color: Colors.black12,
+    offset: Offset(0, 3),
+    blurRadius: 6,
+  ),
+];
+
+kScreenAwareSize(int size, BuildContext context) {
+  double baseHeight = 640.0;
+  return size * MediaQuery.of(context).size.height / baseHeight;
+}
---
+++ lib/pages/splash.dart
@@ -0,0 +1,89 @@
+import "package:flutter/material.dart";
+import 'package:nbase/helpers/constants.dart';
+
+import 'welcome_back.dart';
+
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> with SingleTickerProviderStateMixin {
+  Animation<double> _opacity;
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: Duration(milliseconds: 2500),
+      vsync: this,
+    );
+    _opacity = Tween<double>(begin: 1.0, end: 0.0).animate(_controller)
+      ..addListener(() {
+        setState(() {});
+      });
+    _controller.forward().then((_) {
+      _navigationPage();
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _navigationPage() {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(
+        builder: (_) => WelcomeBack(),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/background.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Container(
+        decoration: BoxDecoration(color: kTransparentYellow),
+        child: SafeArea(
+          child: new Scaffold(
+            body: Column(
+              children: <Widget>[
+                Expanded(
+                  child: Opacity(
+                    opacity: _opacity.value,
+                    child: new Image.asset('assets/logo.png'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: RichText(
+                    text: TextSpan(
+                      style: TextStyle(color: Colors.black),
+                      children: [
+                        TextSpan(text: 'Powered by '),
+                        TextSpan(
+                          text: 'E-Services',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
